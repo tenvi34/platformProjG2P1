c2a01da4f67ab7db55855190fc84a47b83665cfc
logicdrive <logicdrive7@gmail.com>
2024-01-23 15:52:51 +0900

    Feat: Music ViewHandler 구현

--- collectedData/src/main/java/musicStreaming/domain/music/MusicRepository.java
+++ collectedData/src/main/java/musicStreaming/domain/music/MusicRepository.java
@@ -1,5 +1,7 @@
 package musicStreaming.domain.music;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -9,4 +11,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 )
 public interface MusicRepository
     extends PagingAndSortingRepository<Music, Long> {
+    Optional<Music> findByMusicId(Long musicId);
 }
