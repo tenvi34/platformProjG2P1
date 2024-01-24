bd9d5db28f7c5313749f696e47001db4a81f3968
wjdwjd <wjdw637@gmail.com>
2024-01-24 16:06:07 +0900

    deletePlayListMusic, deleteComment

--- playList/src/main/java/musicStreaming/domain/playListMusic/PlayListMusicTasks/DeletePlayListMusicByPlayListDeletedTask.java
+++ playList/src/main/java/musicStreaming/domain/playListMusic/PlayListMusicTasks/DeletePlayListMusicByPlayListDeletedTask.java
@@ -1,10 +1,12 @@
 package musicStreaming.domain.playListMusic.PlayListMusicTasks;
 
-import musicStreaming._global.event.PlayListDeleted;
+import java.util.Optional;
 
+import musicStreaming._global.event.PlayListDeleted;
+import musicStreaming._global.event.PlayListMusicDeleted;
 import musicStreaming._global.logger.CustomLogger;
 import musicStreaming._global.logger.CustomLoggerType;
-
+import musicStreaming.domain.playListMusic.PlayListMusic;
 import musicStreaming.domain.playListMusic.PlayListMusicRepository;
 
 public class DeletePlayListMusicByPlayListDeletedTask {
@@ -13,7 +15,18 @@ public class DeletePlayListMusicByPlayListDeletedTask {
             PlayListMusicRepository playListMusicRepository) {
         CustomLogger.debug(CustomLoggerType.EFFECT, "TODO: deletePlayListMusicByPlayListDeleted");
 
+        Optional<PlayListMusic> optionalPlayListMusic = playListMusicRepository.findById(playListDeleted.getId());
+
         // [1] playListId와 매칭되는 모든 PlayListMusic을 삭제시킵니다.
+        playListMusicRepository.delete(optionalPlayListMusic.get());
         // [2] 각각의 삭제되는 PlayListMusic 마다 PlayListMusicDeleted 이벤트를 발생시킵니다.
+        if (optionalPlayListMusic.isPresent()) {
+            PlayListMusic playListMusic = optionalPlayListMusic.get();
+
+            PlayListMusicDeleted playListMusicDeleted = new PlayListMusicDeleted(playListMusic);
+            playListMusicDeleted.publishAfterCommit();
+        } else {
+            System.out.println("Comment not found");
+        }
     }
 }
