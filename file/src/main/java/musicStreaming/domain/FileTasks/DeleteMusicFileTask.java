59ee5b9af51ce3075c4e185e74d4878272833d58
logicdrive <logicdrive7@gmail.com>
2024-01-24 16:58:40 +0900

    Refactor: 검토 완료

--- file/src/main/java/musicStreaming/domain/FileTasks/DeleteMusicFileTask.java
+++ file/src/main/java/musicStreaming/domain/FileTasks/DeleteMusicFileTask.java
@@ -4,9 +4,9 @@ import java.util.Optional;
 
 import musicStreaming._global.event.MusicFileDeleteRequested;
 import musicStreaming._global.event.MusicFileDeleted;
-import musicStreaming._global.logger.CustomLogger;
-import musicStreaming._global.logger.CustomLoggerType;
+import musicStreaming._global.exceptions.FileNotFoundException;
 import musicStreaming._global.resources.ResourcesService;
+
 import musicStreaming.domain.File;
 import musicStreaming.domain.FileRepository;
 
@@ -14,20 +14,21 @@ public class DeleteMusicFileTask {
     // 요청된 음악 파일을 삭제시키기 위해서
     public static void deleteMusicFileTask(MusicFileDeleteRequested musicFileDeleteRequested,
             FileRepository fileRepository, ResourcesService resourcesService) {
-        CustomLogger.debug(CustomLoggerType.EFFECT, "TODO: deleteMusicFile");
 
         // [1] 주어진 fileId에 해당하는 File 데이터를 얻습니다.
-        Optional<File> opFile = fileRepository.findById(musicFileDeleteRequested.getFileId());
-        File file = opFile.get();
+        Optional<File> optionalFile = fileRepository.findById(musicFileDeleteRequested.getFileId());
+        if(!optionalFile.isPresent()) throw new FileNotFoundException();
+        File fileToDelete = optionalFile.get();
 
         // [2] 주어진 File의 path경로로 resourcesService를 이용해서 파일을 삭제합니다.
-        resourcesService.deleteFile(file.getPath());
+        resourcesService.deleteFile(fileToDelete.getPath());
 
         // [3] 주어진 File도 삭제합니다.
-        fileRepository.delete(file);
+        fileRepository.delete(fileToDelete);
 
         // [4] MusicFileDeleted 이벤트를 발생시킵니다.
-        MusicFileDeleted musicFileDeleted = new MusicFileDeleted(file, file.getId());
+        MusicFileDeleted musicFileDeleted = new MusicFileDeleted(fileToDelete, fileToDelete.getId());
         musicFileDeleted.publishAfterCommit();
+
     }
 }
