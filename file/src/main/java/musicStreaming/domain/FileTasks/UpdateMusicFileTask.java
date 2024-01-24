111fdeae79fa0699ba067dac9b66896c10b62272
hyerim <hyerim>
2024-01-24 22:17:11 +0900

    Feat : UpdateMusicFile

--- file/src/main/java/musicStreaming/domain/FileTasks/UpdateMusicFileTask.java
+++ file/src/main/java/musicStreaming/domain/FileTasks/UpdateMusicFileTask.java
@@ -1,13 +1,18 @@
 package musicStreaming.domain.FileTasks;
 
+import java.util.Optional;
+
 import org.bouncycastle.asn1.dvcs.Data;
 
 import musicStreaming._global.event.MusicFileUpdateRequested;
+import musicStreaming._global.event.MusicFileUpdated;
 import musicStreaming._global.externalSystemProxy.ExternalSystemProxyService;
+import musicStreaming._global.externalSystemProxy.reqDtos.GetDataUrlReqDto;
+import musicStreaming._global.externalSystemProxy.resDtos.GetDataUrlResDto;
 import musicStreaming._global.logger.CustomLogger;
 import musicStreaming._global.logger.CustomLoggerType;
 import musicStreaming._global.resources.ResourcesService;
-
+import musicStreaming.domain.File;
 import musicStreaming.domain.FileRepository;
 
 public class UpdateMusicFileTask {
@@ -19,17 +24,30 @@ public class UpdateMusicFileTask {
         CustomLogger.debug(CustomLoggerType.EFFECT, "TODO: updateMusicFile");
 
         // [1] 얻어진 DataUrlCode를 externalSystemProxyService를 이용해서 요청해서 실제 DataUrl을 얻습니다.
-
-        // String DataUrl = externalSystemProxyService.getDataUrl(musicFileUpdateRequested.getDataUrlCode());
-
+        GetDataUrlReqDto dto = new GetDataUrlReqDto(musicFileUpdateRequested.getDataUrlCode());
+        GetDataUrlResDto getDataDto = null;
+        try {
+            getDataDto = externalSystemProxyService.getDataUrl(dto);
+        } catch (Exception e) { return ;}
+        
+        String DataUrl = getDataDto.getDataUrl();
         // [2] resourcesService로 DataUrl을 저장해서 Path를 얻습니다.
-        //resourcesService.writeMp3FileFromDataUrl(DataUrl);
+        String path = resourcesService.writeMp3FileFromDataUrl(DataUrl);
         
         // [3] fileRepository에서 fildId를 통해서 수정시킬 File 데이터를 얻습니다.
+        Optional<File> file = fileRepository.findById(musicFileUpdateRequested.getFileId());
+        File files = file.get();
         // [4] 기존에 존재하는 파일을 삭제시킵니다.
+        if (file.isPresent()) {
+            resourcesService.deleteFile(files.getPath());
+        }
         // [5] path를 새로운 파일의 path로 수정합니다.
-
+        files.setPath(path);
+        fileRepository.save(files);
         // [6] 총 음악의 길이를 resourcesService로 얻습니다.
+        int musicLength = resourcesService.getMp3TotalSeconds(files.getPath());
         // [7] MusicFileUpdated 이벤트를 발생시킵니다.
+        MusicFileUpdated musicFileUpdated = new MusicFileUpdated(files,files.getId(), musicLength,files.getPath());
+        musicFileUpdated.publishAfterCommit();
     }
 }
