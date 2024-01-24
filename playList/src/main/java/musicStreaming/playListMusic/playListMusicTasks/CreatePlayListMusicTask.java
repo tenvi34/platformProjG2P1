eb37542c0467375458672f8d75934c566fa32355
logicdrive <logicdrive7@gmail.com>
2024-01-24 12:12:58 +0900

    Refactor: 검토 완료

--- playList/src/main/java/musicStreaming/playListMusic/playListMusicTasks/CreatePlayListMusicTask.java
+++ playList/src/main/java/musicStreaming/playListMusic/playListMusicTasks/CreatePlayListMusicTask.java
@@ -1,10 +1,6 @@
 package musicStreaming.playListMusic.playListMusicTasks;
 
-import java.util.Date;
-
 import musicStreaming._global.event.PlayListMusicCreated;
-import musicStreaming._global.logger.CustomLogger;
-import musicStreaming._global.logger.CustomLoggerType;
 
 import musicStreaming.domain.playListMusic.PlayListMusic;
 import musicStreaming.domain.playListMusic.PlayListMusicRepository;
@@ -16,21 +12,19 @@ public class CreatePlayListMusicTask {
     // 주어진 DataURL을 저장하고, File 서비스에 업로드 요청을 수행하기 위해서
     public static CreatePlayListMusicResDto createPlayListMusicTask(CreatePlayListMusicReqDto createPlayListMusicReqDto, Long userId, 
             PlayListMusicRepository playListMusicRepository) {
-        CustomLogger.debug(CustomLoggerType.EFFECT, "TODO: createPlayListMusic");
-
         // [1] playListMusicRepository를 이용해서 새로운 PlayListMusic 데이터를 저장합니다.
-        PlayListMusic playListMusic = new PlayListMusic();
+        PlayListMusic savedPlayListMusic = playListMusicRepository.save(
+            PlayListMusic.builder()
+                .playListId(createPlayListMusicReqDto.getPlayListId())
+                .musicId(createPlayListMusicReqDto.getMusicId())
+                .createrId(userId)
+                .title(createPlayListMusicReqDto.getTitle())
+                .build()
+        );
 
-        playListMusic.setPlayListId(createPlayListMusicReqDto.getPlayListId());
-        playListMusic.setMusicId(createPlayListMusicReqDto.getMusicId());
-        playListMusic.setTitle(createPlayListMusicReqDto.getTitle());
-        playListMusic.setCreaterId(userId);
-
-        PlayListMusic playList = playListMusicRepository.save(playListMusic);
         // [2] PlayListMusicCreated 이벤트를 발생시킵니다.
-        PlayListMusicCreated playListMusicCreated = new PlayListMusicCreated(playList);
-        playListMusicCreated.publishAfterCommit();
-        
-        return new CreatePlayListMusicResDto(playList);
+        (new PlayListMusicCreated(savedPlayListMusic)).publishAfterCommit();
+
+        return new CreatePlayListMusicResDto(savedPlayListMusic);
     }
 }
