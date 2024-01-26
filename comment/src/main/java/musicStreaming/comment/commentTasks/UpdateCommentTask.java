74b7b18201dd7e0b054fa8859b2ba6abd0b47169
wjdwjd <wjdw637@gmail.com>
2024-01-26 10:13:15 +0900

    UpdateCommentTask,UpdateFileIdTask

--- comment/src/main/java/musicStreaming/comment/commentTasks/UpdateCommentTask.java
+++ comment/src/main/java/musicStreaming/comment/commentTasks/UpdateCommentTask.java
@@ -2,6 +2,7 @@ package musicStreaming.comment.commentTasks;
 
 import musicStreaming._global.logger.CustomLogger;
 import musicStreaming._global.logger.CustomLoggerType;
+import musicStreaming._global.event.CommentUpdated;
 
 import musicStreaming.comment.reqDtos.UpdateCommentReqDto;
 import musicStreaming.comment.resDtos.UpdateCommentResDto;
@@ -16,10 +17,18 @@ public class UpdateCommentTask {
         CommentRepository commentRepository) {
         CustomLogger.debug(CustomLoggerType.EFFECT, "TODO: UpdateComment");
 
-        // [1] commentRepository에서 commentId와 일치하는 Comment를 가져옵니다.
-        // [2] Comment를 수정합니다.
-        // [3] CommentUpdated 이벤트를 발생시킵니다.
+      // [1] commentRepository에서 commentId와 일치하는 Comment를 가져옵니다.
+      Long commentId = updateCommentReqDto.getCommentId();
+      Comment comment = commentRepository.findById(commentId)
+              .orElseThrow(() -> new RuntimeException("Comment not found"));
 
-        return new UpdateCommentResDto(new Comment());
+      // [2] Comment를 수정합니다.
+      comment.setContent(updateCommentReqDto.getContent());  // 예시: content를 업데이트
+
+      // [3] CommentUpdated 이벤트를 발생시킵니다.
+      CommentUpdated commentUpdated = new CommentUpdated(comment);
+      commentUpdated.publishAfterCommit();
+
+      return new UpdateCommentResDto(comment);
     }
 }
