4ff00975ff252dc356b916255cdfdba3e5e1380c
logicdrive <122352103+logicdrive@users.noreply.github.com>
2024-01-26 09:25:44 +0900

    Update PlayListMusicDeleted.java
    Rollback

--- playList/src/main/java/musicStreaming/_global/event/PlayListMusicDeleted.java
+++ playList/src/main/java/musicStreaming/_global/event/PlayListMusicDeleted.java
@@ -2,30 +2,31 @@ package musicStreaming._global.event;
 
 import musicStreaming._global.infra.AbstractEvent;
 
-import musicStreaming.domain.playList.PlayList;
-import musicStreaming.sanityCheck.reqDtos.MockPlayListDeletedReqDto;
+import musicStreaming.domain.playListMusic.PlayListMusic;
+
+import musicStreaming.sanityCheck.reqDtos.MockPlayListMusicDeletedReqDto;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-// 플레이 리스트가 삭제됨을 알리는 이벤트
+// 플레이 리스트 음악이 삭제됨을 알리는 이벤트
 @Data
 @ToString
 @EqualsAndHashCode(callSuper=false)
-public class PlayListDeleted extends AbstractEvent {
+public class PlayListMusicDeleted extends AbstractEvent {
     private Long id;
 
-    public PlayListDeleted(PlayList aggregate) {
+    public PlayListMusicDeleted(PlayListMusic aggregate) {
         super(aggregate);
     }
 
-    public PlayListDeleted(MockPlayListDeletedReqDto mockData) {
+    public PlayListMusicDeleted(MockPlayListMusicDeletedReqDto mockData) {
         super();
         this.id = mockData.getId();
     }
 
-    public PlayListDeleted() {
+    public PlayListMusicDeleted() {
         super();
     }
 }
