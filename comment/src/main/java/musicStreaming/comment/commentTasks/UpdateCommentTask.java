4294176bce2d213eb85b63eb05113d7691793a87
logicdrive <logicdrive7@gmail.com>
2024-01-26 11:17:36 +0900

    Refactor: 검토 완료

--- comment/src/main/java/musicStreaming/comment/commentTasks/UpdateCommentTask.java
+++ comment/src/main/java/musicStreaming/comment/commentTasks/UpdateCommentTask.java
@@ -1,8 +1,7 @@
 package musicStreaming.comment.commentTasks;
 
-import musicStreaming._global.logger.CustomLogger;
-import musicStreaming._global.logger.CustomLoggerType;
 import musicStreaming._global.event.CommentUpdated;
+import musicStreaming._global.exceptions.CommentNotFoundException;
 
 import musicStreaming.comment.reqDtos.UpdateCommentReqDto;
 import musicStreaming.comment.resDtos.UpdateCommentResDto;
@@ -15,20 +14,18 @@ public class UpdateCommentTask {
     // 코멘트를 업데이트하기 위해서
     public static UpdateCommentResDto updateCommentTask(UpdateCommentReqDto updateCommentReqDto, 
         CommentRepository commentRepository) {
-        CustomLogger.debug(CustomLoggerType.EFFECT, "TODO: UpdateComment");
 
       // [1] commentRepository에서 commentId와 일치하는 Comment를 가져옵니다.
-      Long commentId = updateCommentReqDto.getCommentId();
-      Comment comment = commentRepository.findById(commentId)
-              .orElseThrow(() -> new RuntimeException("Comment not found"));
+      Comment commentToUpdate = commentRepository.findById(updateCommentReqDto.getCommentId())
+              .orElseThrow(() -> new CommentNotFoundException());     
 
       // [2] Comment를 수정합니다.
-      comment.setContent(updateCommentReqDto.getContent());  // 예시: content를 업데이트
+      commentToUpdate.setContent(updateCommentReqDto.getContent());
+      Comment savedComment = commentRepository.save(commentToUpdate);
 
       // [3] CommentUpdated 이벤트를 발생시킵니다.
-      CommentUpdated commentUpdated = new CommentUpdated(comment);
-      commentUpdated.publishAfterCommit();
+      (new CommentUpdated(savedComment)).publishAfterCommit();
 
-      return new UpdateCommentResDto(comment);
+      return new UpdateCommentResDto(savedComment);
     }
 }
