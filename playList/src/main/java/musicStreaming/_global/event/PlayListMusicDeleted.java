fc65ab1b8cb348746678b8dd6d21b74854d6f03c
logicdrive <122352103+logicdrive@users.noreply.github.com>
2024-01-26 09:25:14 +0900

    Update PlayListMusicDeleted.java
    Rollback

--- playList/src/main/java/musicStreaming/_global/event/PlayListMusicDeleted.java
+++ playList/src/main/java/musicStreaming/_global/event/PlayListMusicDeleted.java
@@ -2,36 +2,30 @@ package musicStreaming._global.event;
 
 import musicStreaming._global.infra.AbstractEvent;
 
-import musicStreaming.domain.playListMusic.PlayListMusic;
-
-import musicStreaming.sanityCheck.reqDtos.MockPlayListMusicDeletedReqDto;
+import musicStreaming.domain.playList.PlayList;
+import musicStreaming.sanityCheck.reqDtos.MockPlayListDeletedReqDto;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-// 플레이 리스트 음악이 삭제됨을 알리는 이벤트
+// 플레이 리스트가 삭제됨을 알리는 이벤트
 @Data
 @ToString
 @EqualsAndHashCode(callSuper=false)
-public class PlayListMusicDeleted extends AbstractEvent {
+public class PlayListDeleted extends AbstractEvent {
     private Long id;
 
-    public PlayListMusicDeleted(PlayListMusic aggregate) {
+    public PlayListDeleted(PlayList aggregate) {
         super(aggregate);
     }
 
-    public PlayListMusicDeleted(MockPlayListMusicDeletedReqDto mockData) {
+    public PlayListDeleted(MockPlayListDeletedReqDto mockData) {
         super();
         this.id = mockData.getId();
     }
 
-    public PlayListMusicDeleted() {
+    public PlayListDeleted() {
         super();
-    }
-
-    public String getPlayListId() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPlayListId'");
     }
 }
