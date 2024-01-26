5d89761fb2dead857fabcde8e9c54e3380c3812f
logicdrive <logicdrive7@gmail.com>
2024-01-26 10:55:12 +0900

    Refactor: 검토 완료

--- music/src/main/java/musicStreaming/domain/MusicTasks/UpdateFileIdTask.java
+++ music/src/main/java/musicStreaming/domain/MusicTasks/UpdateFileIdTask.java
@@ -2,8 +2,8 @@ package musicStreaming.domain.MusicTasks;
 
 import musicStreaming._global.dataUrlStorage.DataUrlStorageService;
 import musicStreaming._global.event.MusicCreated;
-import musicStreaming._global.logger.CustomLogger;
-import musicStreaming._global.logger.CustomLoggerType;
+import musicStreaming._global.exceptions.MusicNotFoundException;
+
 import musicStreaming.domain.Music;
 import musicStreaming.domain.MusicRepository;
 
@@ -12,18 +12,20 @@ public class UpdateFileIdTask {
     // 또한, Music 서비스에 임시로 저장되었던 DataUrlCode 파일을 삭제시키기 위해서
     public static void updateFileIdTask(Long musicId, Long fileId, Integer totalSeconds, String dataUrlCode, 
             MusicRepository musicRepository, DataUrlStorageService dataUrlStorageService) {
-        CustomLogger.debug(CustomLoggerType.EFFECT, "TODO: updateFileId");
 
         // [1] musicRepository를 이용해서 주어진 musicId에 해당하는 Music 클래스를 얻습니다.
-        Music music = musicRepository.findById(musicId).get();
+        Music musicToUpdate = musicRepository.findById(musicId)
+            .orElseThrow(() -> new MusicNotFoundException());
+
         // [2] 얻어진 Music 클래스에서 fileId, totalSeconds를 업데이트하고, 저장합니다.
-        music.setFileId(fileId);
-        music.setTotalSeconds(totalSeconds);
-        musicRepository.save(music);
+        musicToUpdate.setFileId(fileId);
+        musicToUpdate.setTotalSeconds(totalSeconds);
+        Music savedMusic = musicRepository.save(musicToUpdate);
+
+
         // [3] Music 데이터가 최종적으로 생성되었으므로, MusicCreated 이벤트를 발행합니다.
-        MusicCreated musicCreated = new MusicCreated();
-        // TODO: 추후 값 세팅 필요
-        musicCreated.publish();   
+        (new MusicCreated(savedMusic, totalSeconds)).publishAfterCommit();
+
         // [4] dataUrlStorageService를 이용해서 dataUrlCode에 해당하는 파일을 삭제시킵니다.
         dataUrlStorageService.deleteDataUrlFile(dataUrlCode);
 
