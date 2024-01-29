53e21f6100aa7efc9361ab879e7cc888c0fcc102
logicdrive <logicdrive7@gmail.com>
2024-01-30 07:22:02 +0900

    Feat: 상단 바 UI 구현

--- frontend/src/music/info/MusicInfoPage.js
+++ frontend/src/music/info/MusicInfoPage.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+import { JwtTokenContext } from "../../_global/provider/jwtToken/JwtTokenContext";
+
+import TopAppBar from '../../_global/components/TopAppBar';
+import IconNavigationButton from '../../_global/components/button/IconNavigationButton';
+import UserManageButton from '../../_global/components/button/UserManageButton';
 
 const MusicInfoPage = () => {
     const {musicId} = useParams();
+    const {jwtTokenState} = useContext(JwtTokenContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(jwtTokenState.jwtToken === null) {
+            navigate("/user/signIn");
+        }
+    }, [jwtTokenState.jwtToken, navigate])
 
     console.log(musicId)
 
     return (
         <>
-            MusicInfoPage
+            <TopAppBar title="음악 정보">  
+                <UserManageButton sx={{marginRight: "5px"}}/>
+                <IconNavigationButton url="/music/list">
+                    <ArrowBackIcon sx={{fontSize: 40}}/>
+                </IconNavigationButton>
+            </TopAppBar>
         </>
     )
 }
