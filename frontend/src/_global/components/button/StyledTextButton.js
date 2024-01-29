124761fff813bc4054f7f93cc22e375485fec843
logicdrive <logicdrive7@gmail.com>
2024-01-29 15:02:33 +0900

    Refactor: styled 패키지 변경

--- frontend/src/_global/components/button/StyledTextButton.js
+++ frontend/src/_global/components/button/StyledTextButton.js
@@ -1,5 +1,5 @@
 import { Button } from '@mui/material';
-import styled from 'styled-components';
+import { styled } from '@mui/system';
 
 const StyledTextButton = styled(Button)({
     "&&&": {
--- frontend/src/_global/components/textField/StyledTextField.js
+++ frontend/src/_global/components/textField/StyledTextField.js
@@ -1,5 +1,5 @@
 import { TextField } from '@mui/material';
-import styled from 'styled-components';
+import { styled } from '@mui/system';
 
 const StyledTextField = styled(TextField)({
     "& label.Mui-focused": {
