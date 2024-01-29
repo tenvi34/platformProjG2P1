6dc18f1db5ce6e9ff9d8e8372009b9454fea48a0
logicdrive <logicdrive7@gmail.com>
2024-01-30 07:28:33 +0900

    Feat: 상단 바 UI 추가

--- frontend/src/playList/info/PlayListInfoPage.js
+++ frontend/src/playList/info/PlayListInfoPage.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+import { JwtTokenContext } from "../../_global/provider/jwtToken/JwtTokenContext";
+
+import TopAppBar from '../../_global/components/TopAppBar';
+import IconNavigationButton from '../../_global/components/button/IconNavigationButton';
+import UserManageButton from '../../_global/components/button/UserManageButton';
 
 const PlayListInfoPage = () => {
+    const {playListId} = useParams();
+    const {jwtTokenState} = useContext(JwtTokenContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(jwtTokenState.jwtToken === null) {
+            navigate("/user/signIn");
+        }
+    }, [jwtTokenState.jwtToken, navigate])
+
+    console.log(playListId)
 
     return (
         <>
-            PlayListInfoPage
+            <TopAppBar title="플레이 리스트 정보">  
+                <UserManageButton sx={{marginRight: "5px"}}/>
+                <IconNavigationButton url="/playList/list">
+                    <ArrowBackIcon sx={{fontSize: 40}}/>
+                </IconNavigationButton>
+            </TopAppBar>
         </>
     )
 }
