9e7745651ba08c0e8ffa698b2756176fb6b525c8
logicdrive <logicdrive7@gmail.com>
2024-01-29 20:01:00 +0900

    Feat: 음악 재생 버튼 구현

--- frontend/src/music/_global/components/MusicPlayer.js
+++ frontend/src/music/_global/components/MusicPlayer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Stack, Card, Box } from '@mui/material';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -6,6 +6,7 @@ import LinkIcon from '@mui/icons-material/Link';
 import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import PauseIcon from '@mui/icons-material/Pause';
 
 import IconButton from '../../../_global/components/button/IconButton';
 import BoldText from '../../../_global/components/text/BoldText';
@@ -14,17 +15,25 @@ import StyledSlider from '../../../_global/components/slider/StyledSlider';
 import ReactAudioPlayer from 'react-audio-player';
 
 const MusicPlayer = ({musicId, sx, ...props}) => {
-    // audio source
-    console.log(musicId)
+    const audioPlayerRef = useRef();
+    const [isPaused, setIsPaused] = useState(true)
 
     const musicUrl = 'http://localhost:8088/api/file/5510a8d0-1b16-4ce6-8622-d1d39f0c3c36.mp3';
 
+    const onPlayButtonClicked = () => {
+        const audioRef = audioPlayerRef.current.audioEl.current;
+
+        if(audioRef.paused) audioRef.play()
+        else audioRef.pause()
+        setIsPaused(audioRef.paused)
+    }
+    
     return (
         <Card variant="outlined" sx={{padding: 1.5, height: 87, cursor: "pointer", ...sx}} {...props}>
             <Stack>
                 <Box>
-                    <IconButton sx={{float: "left"}} buttonSx={{width: "35px", minWidth: "35px", height: "35px", minHeight: "35px"}} textSx={{}}>
-                        <PlayArrowIcon/>
+                    <IconButton onClick={onPlayButtonClicked} sx={{float: "left"}} buttonSx={{width: "35px", minWidth: "35px", height: "35px", minHeight: "35px"}} textSx={{}}>
+                        {isPaused ? <PlayArrowIcon/> : <PauseIcon/>}
                     </IconButton>
 
                     <Box sx={{float: "left", marginLeft: "5px"}}>
@@ -63,6 +72,8 @@ const MusicPlayer = ({musicId, sx, ...props}) => {
 
             <ReactAudioPlayer
                 src={musicUrl}
+                ref={audioPlayerRef}
+                onEnded={()=>{setIsPaused(true)}}
             />
         </Card>
     )
