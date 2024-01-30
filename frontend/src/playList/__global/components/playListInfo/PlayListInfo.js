302d99fa5d6b86954189295bf497769d75dcecee
logicdrive <logicdrive7@gmail.com>
2024-01-30 13:33:59 +0900

    Feat: 플레이리스트 공유, 삭제 버튼 UI 기반 구현

--- frontend/src/playList/__global/components/playListInfo/PlayListInfo.js
+++ frontend/src/playList/__global/components/playListInfo/PlayListInfo.js
@@ -7,6 +7,8 @@ import EditIcon from '@mui/icons-material/Edit';
 
 import IconButton from '../../../../_global/components/button/IconButton';
 import BoldText from '../../../../_global/components/text/BoldText';
+import YesNoButton from '../../../../_global/components/button/YesNoButton';
+import ShareLinkButton from '../../../../_global/components/button/ShareLinkButton';
 
 const PlayListInfo = ({playListId, sx, ...props}) => {
     const navigate = useNavigate();
@@ -24,13 +26,17 @@ const PlayListInfo = ({playListId, sx, ...props}) => {
                 </Box>
 
                 <Box>
-                    <IconButton sx={{float: "left"}} buttonSx={{width: "35px", minWidth: "35px", height: "18px", minHeight: "18px"}} textSx={{fontSize: "12px", paddingBottom: "8px"}}>
-                        <LinkIcon sx={{width: "15px", height: "15px", float: "left"}}/>
-                    </IconButton>
+                    <ShareLinkButton title="음악 공유 링크" shareUrl="http://localhost:8088/playList/info/1">
+                        <IconButton sx={{float: "left"}} buttonSx={{width: "35px", minWidth: "35px", height: "18px", minHeight: "18px"}} textSx={{fontSize: "12px", paddingBottom: "8px"}}>
+                            <LinkIcon sx={{width: "15px", height: "15px", float: "left"}}/>
+                        </IconButton>
+                    </ShareLinkButton>
 
-                    <IconButton sx={{float: "right", marginLeft: "5px"}} buttonSx={{width: "35px", minWidth: "35px", height: "18px", minHeight: "18px"}} textSx={{fontSize: "12px", paddingBottom: "8px"}}>
-                        <DeleteIcon sx={{width: "15px", height: "15px", float: "left"}}/>
-                    </IconButton>
+                    <YesNoButton onClickYes={()=>{alert("YES")}} title="정말로 삭제하시겠습니까?">
+                        <IconButton sx={{float: "right", marginLeft: "5px"}} buttonSx={{width: "35px", minWidth: "35px", height: "18px", minHeight: "18px"}} textSx={{fontSize: "12px", paddingBottom: "8px"}}>
+                            <DeleteIcon sx={{width: "15px", height: "15px", float: "left"}}/>
+                        </IconButton>
+                    </YesNoButton>
                     <IconButton sx={{float: "right", marginLeft: "5px"}} buttonSx={{width: "35px", minWidth: "35px", height: "18px", minHeight: "18px"}} textSx={{fontSize: "12px", paddingBottom: "8px"}}>
                         <EditIcon sx={{width: "15px", height: "15px", float: "left"}}/>
                     </IconButton>
