59c7fdeee4b361cda7a0ac6dc87e211e83532b7d
logicdrive <logicdrive7@gmail.com>
2024-01-30 13:21:12 +0900

    Feat: 댓글 삭제 버튼 UI 기반 구현

--- frontend/src/music/info/components/CommentInfo.js
+++ frontend/src/music/info/components/CommentInfo.js
@@ -7,6 +7,7 @@ import CommentUpdateButton from './CommentUpdateButton';
 import BoldText from '../../../_global/components/text/BoldText';
 import NormalText from '../../../_global/components/text/NormalText';
 import IconButton from '../../../_global/components/button/IconButton';
+import YesNoButton from '../../../_global/components/button/YesNoButton';
 
 const CommentInfo = () => {
 
@@ -21,10 +22,11 @@ const CommentInfo = () => {
                 <BoldText sx={{float: "left", marginX: "5px"}}>·</BoldText>
                 <BoldText sx={{float: "left", color: "lightgray"}}>2023-01-01</BoldText>
 
-                
-                <IconButton sx={{float: "right", marginLeft: "5px"}} buttonSx={{width: "20px", minWidth: "20px", height: "18px", minHeight: "18px"}} textSx={{fontSize: "12px", paddingBottom: "8px"}}>
-                    <DeleteIcon sx={{width: "15px", height: "15px", float: "left"}}/>
-                </IconButton>
+                <YesNoButton onClickYes={()=>{alert("YES")}} title="정말로 삭제하시겠습니까?">
+                    <IconButton sx={{float: "right", marginLeft: "5px"}} buttonSx={{width: "20px", minWidth: "20px", height: "18px", minHeight: "18px"}} textSx={{fontSize: "12px", paddingBottom: "8px"}}>
+                        <DeleteIcon sx={{width: "15px", height: "15px", float: "left"}}/>
+                    </IconButton>
+                </YesNoButton>
                 <CommentUpdateButton onClickSaveButton={onClickCommentUpdateButton}/>
             </Box>
             <NormalText style={{fontWeight: "1px"}}>
