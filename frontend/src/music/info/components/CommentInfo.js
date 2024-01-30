fc2069343abfcde9213e1655262f82243c527fa6
logicdrive <logicdrive7@gmail.com>
2024-01-30 13:19:51 +0900

    Feat: 댓글 업데이트 버튼 UI 기반 작업

--- frontend/src/music/info/components/CommentInfo.js
+++ frontend/src/music/info/components/CommentInfo.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Stack, Box} from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
+
+import CommentUpdateButton from './CommentUpdateButton';
 
 import BoldText from '../../../_global/components/text/BoldText';
 import NormalText from '../../../_global/components/text/NormalText';
 import IconButton from '../../../_global/components/button/IconButton';
 
 const CommentInfo = () => {
+
+    const onClickCommentUpdateButton = (title) => {
+        alert(title)
+    }
 
     return (
         <Stack>
@@ -16,12 +21,11 @@ const CommentInfo = () => {
                 <BoldText sx={{float: "left", marginX: "5px"}}>·</BoldText>
                 <BoldText sx={{float: "left", color: "lightgray"}}>2023-01-01</BoldText>
 
+                
                 <IconButton sx={{float: "right", marginLeft: "5px"}} buttonSx={{width: "20px", minWidth: "20px", height: "18px", minHeight: "18px"}} textSx={{fontSize: "12px", paddingBottom: "8px"}}>
                     <DeleteIcon sx={{width: "15px", height: "15px", float: "left"}}/>
                 </IconButton>
-                <IconButton sx={{float: "right", marginLeft: "5px"}} buttonSx={{width: "20px", minWidth: "20px", height: "18px", minHeight: "18px"}} textSx={{fontSize: "12px", paddingBottom: "8px"}}>
-                    <EditIcon sx={{width: "15px", height: "15px", float: "left"}}/>
-                </IconButton>
+                <CommentUpdateButton onClickSaveButton={onClickCommentUpdateButton}/>
             </Box>
             <NormalText style={{fontWeight: "1px"}}>
                 Test Comment
