68528594ad542adbc80612bff0053d71d57433ff
logicdrive <logicdrive7@gmail.com>
2024-01-30 11:17:46 +0900

    Feat: 유저 정보 변경 다이얼로그 추가

--- frontend/src/_global/components/button/UserManageButton.js
+++ frontend/src/_global/components/button/UserManageButton.js
@@ -1,16 +1,53 @@
-import { React } from 'react';
+import React, {useState} from 'react';
+import { TextField, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 
 import IconButton from './IconButton';
 
 const UserManageButton = ({...props}) => {
+  const [isDialogOpend, setIsDialogOpend] = useState(false);
+  const [userName, setUserName] = useState("")
+  
+  const onClickSaveButton = () => {
+    alert(userName);
+  }
 
-    return (
-      <IconButton onClick={() => {}} {...props}>
-        <ManageAccountsIcon sx={{fontSize: 35, paddingTop: 0.3, paddingLeft: 0.3}}/>
-      </IconButton>
-    )
+  return (
+    <>
+    <IconButton onClick={()=>{setIsDialogOpend(true);}} {...props}>
+      <ManageAccountsIcon sx={{fontSize: 35, paddingTop: 0.3, paddingLeft: 0.3}}/>
+    </IconButton>
+
+    <Dialog open={isDialogOpend} onClose={()=>{setIsDialogOpend(false);}}>
+      <DialogTitle sx={{color: "black", fontWeight: "bolder", fontFamily: "BMDfont"}}>사용자 정보 수정</DialogTitle>
+      <DialogContent>
+          <TextField
+              name="userName"
+              label="유저명"
+
+              margin="normal"
+              fullWidth
+
+              sx={{width: 400}}
+              value={userName}
+
+              onChange={(e)=>{setUserName(e.target.value)}}
+          />
+      </DialogContent>
+
+      <DialogActions>
+          <Button onClick={() => {
+            setIsDialogOpend(false);
+          }} sx={{color: "black", fontWeight: "bolder", fontFamily: "BMDfont"}}>닫기</Button>
+          <Button onClick={() => {
+            onClickSaveButton();
+            setIsDialogOpend(false);
+          }} sx={{color: "black", fontWeight: "bolder", fontFamily: "BMDfont"}}>저장</Button>
+      </DialogActions>
+    </Dialog>
+    </>
+  )
 }
 
 export default UserManageButton;
