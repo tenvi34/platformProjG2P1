3bab8d170ce1d819a624dcb08429cfd03e6d1c28
logicdrive <logicdrive7@gmail.com>
2024-01-30 14:53:11 +0900

    Feat: 음악 기반, 제작자 기반 검색 기능 추가

--- collectedData/src/main/java/musicStreaming/domain/music/MusicRepository.java
+++ collectedData/src/main/java/musicStreaming/domain/music/MusicRepository.java
@@ -1,5 +1,6 @@
 package musicStreaming.domain.music;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -12,4 +13,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface MusicRepository
     extends PagingAndSortingRepository<Music, Long> {
     Optional<Music> findByMusicId(Long musicId);
+    List<Music> findByTitleContainingIgnoreCase(String title);
+    List<Music> findByCreaterContainingIgnoreCase(String creater);
 }
