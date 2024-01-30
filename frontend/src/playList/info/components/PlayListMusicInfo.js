c5f4de716e20ef8e4e54392334d8ae2f2c8d6703
logicdrive <logicdrive7@gmail.com>
2024-01-30 14:09:01 +0900

    Feat: 플레이 리스트 음악 편집 버튼 UI 기반 구현

--- frontend/src/playList/info/components/PlayListMusicInfo.js
+++ frontend/src/playList/info/components/PlayListMusicInfo.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Card, Box } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
 
 import BoldText from '../../../_global/components/text/BoldText';
 import IconButton from '../../../_global/components/button/IconButton';
 import YesNoButton from '../../../_global/components/button/YesNoButton';
 
+import UpdatePlayListMusicButton from './UpdatePlayListMusicButton';
+
 const PlayListMusicInfo = ({playListMusicId, onClickTitle, sx, ...props}) => {
+
+    const onClickUpdatePlayListMusicButton = (title) => {
+        alert(title)
+    }
 
     return (
         <Card sx={{padding: 1.5, display: "flex", ...sx}} {...props} variant="outlined">
@@ -21,9 +26,8 @@ const PlayListMusicInfo = ({playListMusicId, onClickTitle, sx, ...props}) => {
                         <DeleteIcon sx={{width: "15px", height: "15px", float: "left"}}/>
                     </IconButton>
                 </YesNoButton>
-                <IconButton sx={{float: "right", marginLeft: "5px"}} buttonSx={{width: "35px", minWidth: "35px", height: "18px", minHeight: "18px"}} textSx={{fontSize: "12px", paddingBottom: "8px"}}>
-                    <EditIcon sx={{width: "15px", height: "15px", float: "left"}}/>
-                </IconButton>
+                <UpdatePlayListMusicButton onClickSaveButton={onClickUpdatePlayListMusicButton}/>
+
                 <BoldText sx={{float: "right", color: "lightgray", fontSize: "10px", marginTop: "7px"}}>2024-01-30</BoldText>
             </Box>
         </Card>
