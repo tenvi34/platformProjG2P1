098597c13d9915d82369478aaf29e91649b2fa0f
logicdrive <logicdrive7@gmail.com>
2024-01-30 11:05:28 +0900

    Refactor: PlayListMusicInfo 분리 작업

--- frontend/src/playList/info/PlayListInfoPage.js
+++ frontend/src/playList/info/PlayListInfoPage.js
@@ -1,13 +1,17 @@
 import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
+import { Stack, Divider } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-
 import { JwtTokenContext } from "../../_global/provider/jwtToken/JwtTokenContext";
+
+import PlayListInfo from '../__global/components/PlayListInfo';
 
 import TopAppBar from '../../_global/components/TopAppBar';
 import IconNavigationButton from '../../_global/components/button/IconNavigationButton';
 import UserManageButton from '../../_global/components/button/UserManageButton';
+import MusicInfo from '../../_global/components/MusicInfo/MusicInfo';
+import PlayListMusicInfo from './PlayListMusicInfo';
 
 const PlayListInfoPage = () => {
     const {playListId} = useParams();
@@ -22,6 +26,10 @@ const PlayListInfoPage = () => {
 
     console.log(playListId)
 
+    const onClickPlayListMusicTitle = (playListMusicId) => {
+        alert(playListMusicId)
+    }
+
     return (
         <>
             <TopAppBar title="플레이 리스트 정보">  
@@ -30,6 +38,18 @@ const PlayListInfoPage = () => {
                     <ArrowBackIcon sx={{fontSize: 40}}/>
                 </IconNavigationButton>
             </TopAppBar>
+
+            <Stack>
+                <MusicInfo sx={{width: "95%", marginTop: "5px"}} musicId={1}/>
+                <Divider sx={{marginTop: "5px"}}/>
+
+                <PlayListInfo sx={{marginTop: "5px"}} playListId={1}/>
+                <Divider sx={{marginTop: "5px"}}/>
+
+                <Stack spacing={1} sx={{marginTop: "5px", width: "100%"}}>
+                    <PlayListMusicInfo playListMusicId={1} onClickTitle={onClickPlayListMusicTitle}/>
+                </Stack>
+            </Stack>
         </>
     )
 }
---
+++ frontend/src/playList/info/PlayListMusicInfo.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card, Box } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+import BoldText from '../../_global/components/text/BoldText';
+import IconButton from '../../_global/components/button/IconButton';
+
+const PlayListMusicInfo = ({playListMusicId, onClickTitle, sx, ...props}) => {
+
+    return (
+        <Card sx={{padding: 1.5, display: "flex", ...sx}} {...props}>
+            <Box onClick={() => {onClickTitle(playListMusicId)}} sx={{cursor: "pointer", width: "100%"}}>
+                <BoldText sx={{float: "left", marginTop: "3px"}}>Test Music Title</BoldText>
+            </Box>
+
+            <Box sx={{width: "200px"}}>
+                <IconButton sx={{float: "right", marginLeft: "5px"}} buttonSx={{width: "35px", minWidth: "35px", height: "18px", minHeight: "18px"}} textSx={{fontSize: "12px", paddingBottom: "8px"}}>
+                    <DeleteIcon sx={{width: "15px", height: "15px", float: "left"}}/>
+                </IconButton>
+                <IconButton sx={{float: "right", marginLeft: "5px"}} buttonSx={{width: "35px", minWidth: "35px", height: "18px", minHeight: "18px"}} textSx={{fontSize: "12px", paddingBottom: "8px"}}>
+                    <EditIcon sx={{width: "15px", height: "15px", float: "left"}}/>
+                </IconButton>
+                <BoldText sx={{float: "right", color: "lightgray", fontSize: "10px", marginTop: "7px"}}>2024-01-30</BoldText>
+            </Box>
+        </Card>
+    )
+}
+
+export default PlayListMusicInfo;
